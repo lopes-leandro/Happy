6f8756f4b482838fa5326df62bf05ff4c4d96de1
CarolDill <carol37_dill@msn.com>
2020-10-17 16:50:29 -0700

    NLW - Modifications
    Added a shortcut for Home Page.
    Finished ternary operator.
    Home page with different background with option to change color!

---
+++ public/scripts/page-landing.js
@@ -0,0 +1,9 @@
+particlesJS.load('page-landing', 'scripts/particlesjs-config.json');
+
+function changeClass() {
+    document.getElementById('page-landing').id = "particles-container";
+    document.getElementById('container').id = "container2"
+}
+
+
+
